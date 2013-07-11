f1288527c5c4270021b0530513d5b68901993d62
lxzhh <lxzhh.hi@163.com>
2013-07-11 10:51:35 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save{ |user| user.email = email.downcase }
+  before_save :create_remember_token
   validates :name, presence: true ,
                     length: {  maximum: 50 }
                     
@@ -29,4 +30,9 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
    
    
+  private
+
+  def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
