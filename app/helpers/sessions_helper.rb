0dd2e330414c95fee971a210049300d8635c1ba1
lxzhh <lxzhh.hi@163.com>
2013-07-17 00:09:14 +0800

    Finish update user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,41 @@
 module SessionsHelper
   def sign_in(user)
+      puts "sign_in "
       cookies.permanent[:remember_token] = user.remember_token
       self.current_user = user
   end
     
   def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
-  end
+      self.current_user = nil
+      cookies.delete(:remember_token)
+    end
   
   def signed_in?
+      puts "signed_in3?  #{@current_user}"
       !current_user.nil?
+      
   end
     
   def current_user=(user)
+    puts "current_user=3  #{user}"
       @current_user = user
   end
   
   def current_user
       @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+
+  end
+  
+  def current_user?(user)
+      user == current_user
+  end
+  
+  def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+    end
+
+  def store_location
+      session[:return_to] = request.fullpath
   end
 end
